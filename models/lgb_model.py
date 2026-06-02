95177a87bfc88635eb32171b753b384e22c6db1d
CORAL <coral@local>
2026-06-03 00:41:14 +0800

    LGB num_leaves 31→63: deeper trees capture higher-order rank_cs interactions beyond pairwise products. Ridge gets all 261 features linearly; trees need depth to find 3-way+ nonlinear splits. Expected +0.0003-0.0008 if third-order interactions have signal

--- models/lgb_model.py
+++ models/lgb_model.py
@@ -10,7 +10,7 @@ import lightgbm as lgb
 class LGBModel:
     def __init__(self):
         self.max_rows = int(os.environ.get("MEOW_LGB_MAX_ROWS", "800000"))
-        self.num_leaves = int(os.environ.get("MEOW_LGB_NUM_LEAVES", "31"))
+        self.num_leaves = int(os.environ.get("MEOW_LGB_NUM_LEAVES", "63"))
         self.learning_rate = float(os.environ.get("MEOW_LGB_LEARNING_RATE", "0.05"))
         self.n_estimators = int(os.environ.get("MEOW_LGB_N_ESTIMATORS", "200"))
         self.extra_trees = os.environ.get("MEOW_LGB_EXTRA_TREES", "0") != "0"
