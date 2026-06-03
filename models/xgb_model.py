fe1122f4c40968d69d6ff22b2f792e5f907544f1
CORAL <coral@local>
2026-06-03 18:16:22 +0800

    XGB reservoir 500K->600K: rank:pairwise benefits from more pairwise comparisons within each (date,interval) group. +20% rows = denser group sampling. 600K rows with float32 features (~200 cols) adds ~40MB over 500K baseline.

--- models/xgb_model.py
+++ models/xgb_model.py
@@ -14,7 +14,7 @@ import xgboost as xgb
 
 class XGBModel:
     def __init__(self):
-        self.max_rows = int(os.environ.get("MEOW_XGB_MAX_ROWS", "500000"))
+        self.max_rows = int(os.environ.get("MEOW_XGB_MAX_ROWS", "600000"))
         self.max_depth = int(os.environ.get("MEOW_XGB_MAX_DEPTH", "6"))
         self.learning_rate = float(os.environ.get("MEOW_XGB_LEARNING_RATE", "0.05"))
         self.n_estimators = int(os.environ.get("MEOW_XGB_N_ESTIMATORS", "200"))
